702ca6fe02f1179282fa1d888360d6da31825dd5
ClaudioLillo <cilillo@uc.cl>
2022-06-14 01:43:59 -0400

    navigator prototype

--- task-planner/src/App.js
+++ task-planner/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
 import Card from './components/card/Card'
+import Navigator from './components/navigator/Navigator'
 
+// data de prueba para testear componentes, se deben paginar cada 12 (1, 2, 3, 4, 6, 12)
+const data = [
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+  {title: 'titulo 1', content: 'contenido'},
+]
 function App() {
   return (
-    <div className="App">
-      <Card title={'tarea 1'} content={'contenido 1'}/>
+    <div className="app">
+      <Navigator/>
+      <div className="cards">
+        {data.map((card, index)=>
+          <Card key={index} title={card.title} content={card.content}/>
+        )}
+      </div>
     </div>
   );
 }
---
+++ task-planner/src/components/navigator/Navigator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Navigator.css'
+
+export default function Navigator(){
+    return(
+        <div className='navigator'>
+            <h1>Planificador de Tareas v0.1</h1>
+        </div>
+    )
+}
